0b6b1cc434e64033f3ea8a733cda225ca2f8ad2c
Kaike Yudi <kaikeyudi@outlook.com>
2022-08-09 19:11:35 -0300

    req-9

---
+++ src/Monster.ts
@@ -0,0 +1,28 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  constructor(
+    private _lifePoints: number = 85, 
+    private _strength: number = 63,
+  ) { }
+
+  get lifePoints() { 
+    return this._lifePoints; 
+  }
+
+  get strength() { 
+    return this._strength; 
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints > 0) { this._lifePoints -= attackPoints; }
+    if (this._lifePoints <= 0) { this._lifePoints = -1; }
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Monster;
